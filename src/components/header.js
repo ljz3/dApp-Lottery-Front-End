4a42cb939d44cfb415868bacfe86ca4264d42d5d
ljz3 <kevin.ottawa@hotmail.com>
2021-09-16 12:02:02 -0400

    Added starter code

---
+++ src/components/header.js
@@ -0,0 +1,49 @@
+import './App.css';
+import {
+  Avatar,
+  Blockie,
+  Box,
+  Button,
+  Card,
+  Checkbox,
+  Field,
+  Flash,
+  Flex,
+  Heading,
+  Icon,
+  Image,
+  Input,
+  Link,
+  Loader,
+  MetaMaskButton,
+  UPortButton,
+  Modal,
+  Pill,
+  Progress,
+  QR,
+  Radio,
+  Select,
+  EthAddress,
+  Slider,
+  Table,
+  Text,
+  Textarea,
+  ToastMessage,
+  Tooltip,
+  BaseStyles,
+  ThemeProvider,
+  theme,
+} from 'rimble-ui'
+
+
+function App() {
+  return (
+    <div className="App">
+    <Box>
+      <Text>Icon component from rimble-ui</Text>
+    </Box>
+    </div>
+  );
+}
+
+export default App;
